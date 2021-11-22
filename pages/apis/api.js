6d1cd27cbdca34e632ede3521bb8fbf1b043441d
nitoc <daren2track@gmail.com>
2021-11-22 10:47:06 +0100

    refactor: updated pack.json

--- pages/apis/api.js
+++ pages/apis/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const register= payload=> axios.post('http://localhost:2000/register',payload)
-export const login= payload=> axios.post('http://localhost:2000/login ',payload)
-export const forgotPassword= payload=> axios.post('http://localhost:2000/forgotPassword ',payload)
-export const deposit= payload=> axios.patch('http://localhost:2000/deposit ',payload)
-export const reset= payload=> axios.patch('http://localhost:2000/reset',payload)
-export const withdraw= payload=> axios.patch('http://localhost:2000/withdraw',payload)
-export const addressfunc= payload=> axios.get('http://localhost:2000/address')
+export const register= payload=> axios.post('https://keytrade.herokuapp.com/register',payload)
+export const login= payload=> axios.post('https://keytrade.herokuapp.com/login ',payload)
+export const forgotPassword= payload=> axios.post('https://keytrade.herokuapp.com/forgotPassword ',payload)
+export const deposit= payload=> axios.patch('https://keytrade.herokuapp.com/deposit ',payload)
+export const reset= payload=> axios.patch('https://keytrade.herokuapp.com/reset',payload)
+export const withdraw= payload=> axios.patch('https://keytrade.herokuapp.com/withdraw',payload)
+export const addressfunc= payload=> axios.get('https://keytrade.herokuapp.com/address')
