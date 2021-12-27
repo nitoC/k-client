e836591ddd3a100ff0154f0972613543b0f00898
chinedu <daren2track@gmail.com>
2021-12-27 13:12:42 +0100

    feature: add referral section

--- apis/api.js
+++ apis/api.js
@@ -5,7 +5,7 @@ export const register= payload=> axios.post('https://keytrade.herokuapp.com/regi
 export const refer= payload=> axios.post('https://keytrade.herokuapp.com/referral',payload)
 export const login= payload=> axios.post('https://keytrade.herokuapp.com/login ',payload)
 export const forgotPassword= payload=> axios.post('https://keytrade.herokuapp.com/forgotPassword ',payload)
-export const referfunc= payload=> axios.post('https://keytrade.herokuapp.com/referral',payload)
+export const referfunc= payload=> axios.post('https://keytrade.herokuapp.com/get-referral',payload)
 //patch endpoints
 export const deposit= payload=> axios.patch('https://keytrade.herokuapp.com/deposit ',payload)
 export const reset= payload=> axios.patch('https://keytrade.herokuapp.com/reset',payload)
